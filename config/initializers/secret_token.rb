cad4964e30090fcda4f1dfc05e8ecf7a147f15b2
clement <clement.perrin.speights@gmail.com>
2012-04-25 06:21:26 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clem::Application.config.secret_token = 'c3643f54706c7ea23d014fd0e7b63085d445ecfde20cc4370cf87f2991976dcd6b24dcf88e62b3ece2beeb345dea5966e10065530c007e7158c6d2372edeac83'
